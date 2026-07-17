3efb652647754bab44d3e873c370e8058d219b80
Swathi H <hanumanthuswathi24@gmail.com>
2026-07-17 19:36:05 +0530

    Add contract_documents table

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -8,3 +8,4 @@ from app.models.role import Role
 from app.models.vendor_category import VendorCategory
 from app.models.vendor_contact import VendorContact
 from app.models.activity_log import ActivityLog
+from app.models.contract_document import ContractDocument
